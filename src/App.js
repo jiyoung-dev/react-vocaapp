d7afa2c0b4c8c001c541644cbead0769e6912017
jiyoung-dev <janice10508@gmail.com>
2022-06-04 00:36:30 +0900

    #5. module css 작성 연습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hello from "./component/Hello";
 import Welcome from './component/Welcome';
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <div className={styles.box}> Hello~ </div>
       <Welcome/>
     </div>
   );
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -7,12 +7,24 @@ export default Hello;
 */
 
 import World from "./World";
+import styles from './Hello.module.css';
 
 export default function Hello() {
     return (
         <>
-            <p>Hello!!</p>
+            <p
+                style = {{
+                    color: "#f00",
+                    borderRight:  "12px solid #000",
+                    marginBottom: "30px",
+                    opacity: 0.5,
+                }}
+                
+            >
+                Hello!!
+            </p>
             <World/>
+            <div className={styles.box}> Hello~ </div>
         </>
     ); 
 }
