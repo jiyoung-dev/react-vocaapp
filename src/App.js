58f1d2769f5f84e375d8666fe9f6f22f82af1ef6
jiyoung-dev <janice10508@gmail.com>
2022-06-04 01:17:34 +0900

    remove

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Hello from "./component/Hello";
-import Welcome from './component/Welcome';
-import styles from "./App.module.css";
 
 function App() {
   return (
     <div className="App">
       <Hello/>
-      <div className={styles.box}> Hello~ </div>
-      <Welcome/>
     </div>
   );
 }
