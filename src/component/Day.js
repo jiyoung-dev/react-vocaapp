7fc94cde7eb9c83f468d0dd72141698e0ab1cb35
Jiyoung <janice10508@gmail.com>
2022-06-18 14:27:35 +0900

    day.js 이전/다음 날짜이동 버튼 구현

--- src/component/Day.js
+++ src/component/Day.js
@@ -3,13 +3,28 @@
 */
 
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import Word from "./Word";
 
 export default function Day() {
     const { day } = useParams();
+    const days = useFetch("http://localhost:3001/days"); 
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
-  
+    const navigate = useNavigate();
+    const nDay = Number(day) + 1;
+    const pDay = Number(day) - 1;
+
+    // 다음날짜 단어장으로 이동 
+    function nextDay() {
+        navigate(`/day/${nDay}`);
+    }
+
+    // 이전날짜 단어장으로 이동 
+    function preDay() {
+        navigate(`/day/${pDay}`);
+    }
+
     return (
         <>
             <h2>Day {day}</h2>
@@ -21,6 +36,28 @@ export default function Day() {
                     ))}
                 </tbody>
             </table>
+            <div
+                style={{
+                    marginTop: 30,
+                    display: 'flex',
+                    justifyContent : 'center',
+                }}
+            >
+                {Number(day) > 1 &&   // day2부터 이전버튼 보이기 
+                    <button 
+                        style={{
+                            margin : 10,  // [top, right, bottom, left]
+                        }}
+                        onClick={preDay}>◀ Day {pDay}</button>
+                }
+                {Number(day) != days.length &&
+                    <button 
+                        style={{
+                            margin : 10,  // [top, right, bottom, left]
+                        }}
+                        onClick={nextDay}>Day {nDay} ▶</button>
+                }
+            </div>
         </>
     )
 }
